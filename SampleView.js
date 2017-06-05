0476f5030c7e95a89288497ef12bfa7c7a494057
nico <nicolas.roux@aspectize.com>
2017-06-05 17:53:24 +0200

    Remove filter

--- SampleView.js
+++ SampleView.js
@@ -6,14 +6,17 @@
 //vTranslations.UploadExcelFile.OnFileSelected.BindCommand(aas.Services.Server.MyTranslationManagerService.ImportTraductionFromExcel(vTranslations.UploadExcelFile.SelectedFile));
 //vTranslations.BtnSave.click.BindCommand(aas.Services.Server.MyTranslationManagerService.SaveTranslations(aas.Data.TranslationData), aas.Data.TranslationData, true, true);
 //vTranslations.TxtSearch.keyup.BindCommand(aas.Services.Browser.UIService.SetCustomFilter, { controlName: aas.ViewName.Translations.GridTranslation, customFilter: aas.Expression('(Key).toLowerCase().indexOf("' + vTranslations.TxtSearch.value + '".toLowerCase()) !== -1') });
-//vTranslations.CheckBoxDisplayNew.click.BindCommand(aas.Services.Browser.UIService.SetCustomFilter(aas.ViewName.Translations.GridTranslation, aas.Expression(IIF(aas.View.Translations.CheckBoxDisplayNew.checked, 'IsNew', ''))));
 //vTranslations.TranslationKey.BindData(aas.Data.TranslationData.Translation.Key);
-//vTranslations.GridTranslation.BindGrid(aas.Data.TranslationData.Translation, 'IsNew ASC, Key ASC');
+//vTranslations.GridTranslation.BindGrid(aas.Data.TranslationData.Translation, 'Key ASC');
 //vTranslations.GridTranslation.PageSize.BindData(20);
 
 //var cClassName = vTranslations.GridTranslation.AddGridColumn('ClassName', 'RowClass');
-//cClassName.className.BindData(aas.Expression(IIF(vTranslations.GridTranslation.DataSource.IsNew, 'bg-success', IIF(vTranslations.GridTranslation.DataSource.Ignore, IIF(aas.View.Translations.CheckBoxDisplayIgnore.checked, 'bg-danger', 'hidden'), ''))));
+//cClassName.className.BindData(aas.Expression(IIF(vTranslations.GridTranslation.DataSource.IsNew, 'bg-success', IIF(vTranslations.GridTranslation.DataSource.Ignore, 'bg-danger', ''))));
 
+//var cIgnore = vTranslations.GridTranslation.AddGridColumn('Ignore', 'SpanCheck');
+//cIgnore.Text.BindData(vTranslations.GridTranslation.DataSource.Ignore);
+//var cIsNew = vTranslations.GridTranslation.AddGridColumn('New', 'SpanCheck');
+//cIsNew.Text.BindData(vTranslations.GridTranslation.DataSource.IsNew);
 //var cName = vTranslations.GridTranslation.AddGridColumn('Translation', 'Span');
 //cName.Text.BindData(vTranslations.GridTranslation.DataSource.Key);
 //var cIgnore = vTranslations.GridTranslation.AddGridColumn('Ignore', 'Button');
